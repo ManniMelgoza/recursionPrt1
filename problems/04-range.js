83b1d4e9c6ee800a74d06e7b178b1a1abd4b7425
Manni <emmanuelmelgoza1@gmail.com>
2024-04-10 20:52:06 -0600

    solved 04

--- problems/04-range.js
+++ problems/04-range.js
@@ -2,19 +2,36 @@
 Write a recursive function, `range`, that takes a start and an end and returns
 an array of all numbers in that range, exclusive. If the end number is less than
 the start, return an empty array.
-
-Examples:
-
-range(1, 5); // [1, 2, 3, 4]
-range(3, 4); // [3]
-range(7, 6); // []
 ***********************************************************************/
-
-
 function range(start, end) {
-  // Your code here 
-}
+  debugger
 
+  //If the end number is less than the start, return an empty array.
+
+    if(start < end) {
+      return [start, ...range(start + 1, end)]
+    }
+    return [];
+  }
+
+  // let rangeArr = [];
+
+  // if(end < start) return []
+
+  // if(rangeArr.length === end - 1) return rangeArr;
+
+  // else if(start < end) {
+  //   rangeArr.push(start)
+  //   start++;
+  //   return range(start, end)
+  // }
+  // return rangeArr;
+// };
+
+
+console.log(range(1, 5)); // [1, 2, 3, 4]
+console.log(range(3, 4)); // [3]
+console.log(range(7, 6)); // []
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
