9b8d94c2ac123208adb9d7cc3751a7ad2d143169
DoctorDoofen <and.bennett0930@gmail.com>
2024-04-10 21:18:01 -0500

    03 solved

--- problems/02-sum-array.js
+++ problems/02-sum-array.js
@@ -9,15 +9,15 @@ sumArray([1, 2, 3]); //  6
 sumArray([0, 1, -3]); //  -2
 sumArray([1, 2, 3, 4, 5]); // 15
 ***********************************************************************/
-debugger
 function sumArray(arr) {
   // Your code here
 
   if (arr.length < 1) return 0;
 
-    return arr.pop() + sumArray(arr)
+  return arr.pop() + sumArray(arr)
 };
 
+debugger
 console.log(sumArray([1, 2, 3])); //  6
 console.log(sumArray([0, 1, -3])); //  -2
 console.log(sumArray([1, 2, 3, 4, 5])); // 15
